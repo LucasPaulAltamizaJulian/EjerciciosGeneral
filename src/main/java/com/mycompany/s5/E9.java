dcd46eebd77d17be8e92a5dd8a0ef9d6e8b0cc4c
LucasPaulAltamizaJulian <114025272+LucasPaulAltamizaJulian@users.noreply.github.com>
2022-10-04 18:22:00 -0500

    NETBEAANS

---
+++ src/main/java/com/mycompany/s5/E9.java
@@ -0,0 +1,27 @@
+/*9. Pedir dos números y mostrarlos ordenados de mayor a menor.
+*/
+package com.mycompany.s5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lucas
+ */
+public class E9 {
+    public static void main(String[] args) {
+        //<editor-fold defaultstate="collapsed" desc="comment">
+        Scanner sc=new Scanner(System.in);
+        int n1,n2;
+        System.out.print("Introduce un número: ");
+        n1=sc.nextInt();
+        System.out.print("Introduce otro número: ");
+        n2=sc.nextInt();
+        
+        if(n1>n2)
+            System.out.println(n1 + " y " + n2);
+        else
+            System.out.println(n2 + " y " + n1);
+//</editor-fold>
+    }
+}
